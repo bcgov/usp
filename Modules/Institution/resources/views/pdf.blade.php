3cf22759682aa015f36eac092bcb39eac69ce653
Kal Marsh <kal.marsh@gov.bc.ca>
2024-02-14 07:26:34 -0800

    renames signature images

--- Modules/Institution/resources/views/pdf.blade.php
+++ Modules/Institution/resources/views/pdf.blade.php
@@ -71,7 +71,7 @@
                 provincial attestation letter, or meet an exception as outlined in the Ministerial Instructions, will
                     not be accepted for processing.</strong> Please refer to <a href="https://www.canada.ca/en/immigration-refugees-citizenship/services/study-canada.html">IRCC’s web site</a> for study permit application information.
 
-                <br/><br/>John Marchesan,<br/><img src="{{ public_path('/images/attestation_sign.png') }}"><br/><br/>
+                <br/><br/>John Marchesan,<br/><img src="{{ public_path('/images/signature/v1.png') }}"><br/><br/>
             </td>
         </tr>
 
