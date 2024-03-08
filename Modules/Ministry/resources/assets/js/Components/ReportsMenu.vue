719616d914725ca4dc0ab215cbbcefd83ebf0f11
Kal Marsh <kal.marsh@gov.bc.ca>
2024-03-07 21:36:59 -0800

    adds report sources

--- Modules/Ministry/resources/assets/js/Components/ReportsMenu.vue
+++ Modules/Ministry/resources/assets/js/Components/ReportsMenu.vue
@@ -2,7 +2,8 @@
 
     <div class="list-group">
         <Link href="/ministry/reports/summary" class="list-group-item list-group-item-action" :class="page.split('summary').length > 1 ? 'active' : ''">Executive Summary</Link>
-        <Link href="/ministry/reports/detail" class="list-group-item list-group-item-action" :class="page.split('detail').length > 1 ? 'active' : ''">Detailed Reporting</Link>
+        <Link href="/ministry/reports/detail" class="list-group-item list-group-item-action" :class="page.split('detail').length > 1 ? 'active' : ''">Report with % Used</Link>
+        <Link href="/ministry/reports/sources" class="list-group-item list-group-item-action" :class="page.split('sources').length > 1 ? 'active' : ''">Report Sources</Link>
     </div>
 </template>
 <script setup>
