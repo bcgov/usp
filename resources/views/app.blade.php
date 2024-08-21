d1cc66895e32a8bfe004f05cfefcac502037c17e
Kal Marsh <kal.marsh@gov.bc.ca>
2024-08-21 08:31:21 -0700

    fixes cookie security

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -27,14 +27,6 @@
         <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQ1HZ6KYMH"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
 
-            gtag('config', 'G-FQ1HZ6KYMH');
-        </script>
     </body>
 </html>
