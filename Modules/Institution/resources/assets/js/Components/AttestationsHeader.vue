871777565f4b9dbb2d63bc9257b1ba10a315434d
AimeeGao <Aimee.Gao@gov.bc.ca>
2026-03-03 08:48:18 -0800

    Fix sorting UI display

--- Modules/Institution/resources/assets/js/Components/AttestationsHeader.vue
+++ Modules/Institution/resources/assets/js/Components/AttestationsHeader.vue
@@ -38,7 +38,7 @@
         <th scope="col" style="min-width: 115px;">
             <a href="#" @click="switchSort('issue_date')">
                 <span>Issue Date</span>
-                <em v-if="sortClmn === 'created_at' && sortType === 'desc'" class="bi bi-sort-numeric-up"></em>
+                <em v-if="sortClmn === 'issue_date' && sortType === 'desc'" class="bi bi-sort-numeric-up"></em>
                 <em v-else class="bi bi-sort-numeric-down"></em>
             </a>
         </th>
